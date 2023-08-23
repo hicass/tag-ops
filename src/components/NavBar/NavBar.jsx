fc46c8125d1d2b5b4672402f3c264b84af9fd32c
hicass <cassidyjwalters@gmail.com>
2023-08-23 12:02:46 -0700

    Remove unused Pages and Components, create a Landing Page,  set up Nav Bar and Landing Page to be displayed upon first loading the site, and add the company logo.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,20 +1,18 @@
 import { Link } from 'react-router-dom';
-import * as userService from '../../utilities/users-service';
+import smallPurpleLogo from '../../assets/images/logos/tag-sm-logo-purple.png';
+import './NavBar.css'
+
 
 export default function NavBar({ user, setUser }) {
-  function handleLogOut() {
-    userService.logOut();
-    setUser(null);
-  }
-
   return (
     <nav>
-      <Link to="/orders">Order History</Link>
-      &nbsp; | &nbsp;
-      <Link to="/orders/new">New Order</Link>
-      &nbsp;&nbsp;
-      <span>Welcome, {user.name}</span>
-      &nbsp;&nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
+      <img id='nav-logo' src={smallPurpleLogo} alt='Tag Operations Logo' />
+      <div>
+        <Link to='/about'>About Us</Link>
+        <Link to='/services'>Services</Link>
+        <Link to='/prices'>Prices</Link>
+        <Link to='/contact'>Contact</Link>
+      </div>
     </nav>
   );
 }
