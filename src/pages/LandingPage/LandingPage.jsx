6a18bfbd7195439f61dd9715ff3d83b2d70ea22c
hicass <cassidyjwalters@gmail.com>
2023-08-24 10:56:19 -0700

    Edit styling and layout of the top section of the landing page.

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -7,11 +7,13 @@ export default function LandingPage() {
         <section id='landing-page'>
             <div id='landing-page-intro'>
                 <h1 id='secret-title'>Tag Operations</h1>
-                <img src={widePurpleLogo} alt='Tag Operations' />
-                <div>
-                <span id='intro-txt-top'>Your tech forward, embedded and flexible back office team;</span><br />
-                <span id='intro-txt-bottom'>bringing you expert level knowledge, automated processes, and clear policies.</span>
+                <div id='intro-txt-section'>
+                    <h2 id='intro-h2'>Simplify, Optimize, Thrive with Tag Operations!</h2>
+                    <div id='txt-container'>
+                        <span id='intro-txt'>Your tech forward, embedded and flexible back office team!</span><br />
+                    </div>
                 </div>
+                <img id='intro-logo' src={widePurpleLogo} alt='Tag Operations' />
             </div>
 
             <div id='bob-section'>
@@ -24,7 +26,7 @@ export default function LandingPage() {
             </div>
 
             <div id='landing-solutions'>
-                <h2> Our Solutions</h2>
+                <h2>Our Solutions</h2>
             </div>
         </section>
     )
