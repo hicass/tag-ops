6df90b1df1018d241776d5edb3ab991c7356e5e5
hicass <cassidyjwalters@gmail.com>
2023-08-29 16:20:27 -0700

    Finish the styling for the form on contact page.

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -8,8 +8,8 @@ export default function ContactPage() {
                 <h1 id='contact-h1'>Contact</h1>
 
                 <p className='added-txt' id='contact-page-txt'>
-                    Welcome to our Contact Page! If you've made it this far, it means our mission to support your business operations 
-                    has struck a chord with you. Whether your business is large or small, we're here to support you in achieving your 
+                    If you've made it this far, it means our mission to support your business
+                    has struck a chord with you. Whether your business is large or small, we're here to assist you in achieving your 
                     operational goals. Let's start a conversation and build a relationship to understand how we can best serve you.
                 </p>
             </div>
@@ -35,25 +35,25 @@ export default function ContactPage() {
                         <p className='form-label'>Service</p>
                         <label className='form-label' id='service-select'>
                             Human Resources
-                            <input value='Human Resources' type='checkbox' />
+                            <input className='form-checkbox' value='Human Resources' type='checkbox' />
                         </label>
                         <label className='form-label' id='service-select'>
                             Operations
-                            <input value='Operations' type='checkbox' />
+                            <input className='form-checkbox' value='Operations' type='checkbox' />
                         </label>
                         <label className='form-label' id='service-select'>
                             Accounting/ Bookkeeping
-                            <input value='Accounting/ Bookkeeping' type='checkbox' />
+                            <input className='form-checkbox' value='Accounting/ Bookkeeping' type='checkbox' />
                         </label>
                     </fieldset>
                 </div>
 
                 <label className='form-label'>
                     Message
-                    <textarea></textarea>
+                    <textarea className='form-input' id='form-textarea'></textarea>
                 </label>
                 
-                <input type='submit' />
+                <input id='form-submit' type='submit' />
             </form>
         </section>
     )
